1a0e560fc8fa26cb5d751e0cb49c4d88abef7254
daneFram <dsears@hawaii.edu>
2022-04-21 19:53:38 -1000

    brought over weight class

---
+++ Weight.cpp
@@ -0,0 +1,209 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205-lab12a-fatCat - EE 205 - Spr 2022
+///
+/// @file Weight.cpp
+/// @version 1.0
+///
+/// @author Dane Sears <dsears@hawaii.edu>
+/// @date   16_APR_2022
+///////////////////////////////////////////////////////////////////////////////
+#include <cassert>
+#include <iostream>
+#include <stdexcept>
+#include <iomanip>
+
+#include "Weight.h"
+
+using namespace std;
+
+static const float UNKNOWN_WEIGHT = 0;
+static const float KILOS_IN_A_POUND = .453592;
+static const float SLUGS_IN_A_POUND = .031081;
+
+static const std::string POUND_LABEL = "Pound" ;
+static const std::string KILO_LABEL = "Kilo";
+static const std::string SLUG_LABEL = "Slug";
+
+
+float pound, slug;
+
+
+float Weight::fromKilogramToPound( const float kilogram ) noexcept {
+return kilogram / KILOS_IN_A_POUND ;
+}
+
+float Weight::fromPoundToKilogram(const float pound) noexcept {
+return pound * KILOS_IN_A_POUND;
+}
+
+float Weight::fromSlugToPound(const float slug) noexcept {
+return slug / SLUGS_IN_A_POUND;
+}
+
+float Weight::fromPoundToSlug(const float slug) noexcept {
+return slug * SLUGS_IN_A_POUND;
+}
+
+bool Weight::isWeightValid(float checkWeight) noexcept {
+
+if ((checkWeight > 0) && (checkWeight < maxWeight)) {
+return true;
+}
+return false;
+}
+///static float convertWeight( float fromWeight, Weight::UnitOfWeight fromUnit,Weight::UnitOfWeight toUnit ) noexcept;
+float Weight::convertWeight(float fromWeight, Weight::UnitOfWeight fromUnit, Weight::UnitOfWeight toUnit) noexcept {
+switch(fromUnit){
+case POUND:
+switch (toUnit) {
+case POUND:
+return fromWeight;
+case KILO:
+return Weight::fromPoundToKilogram(fromWeight);
+case SLUG:
+return Weight::fromPoundToSlug(fromWeight);
+}
+case KILO:
+switch (toUnit) {
+case POUND:
+return fromKilogramToPound(fromWeight);
+case KILO:
+return fromWeight;
+case SLUG:
+return fromSlugToPound(fromKilogramToPound(fromWeight));
+}
+case SLUG:
+switch (toUnit){
+case POUND:
+return fromSlugToPound(fromWeight);
+case KILO:
+return fromPoundToKilogram(fromSlugToPound(fromWeight));
+case SLUG:
+return fromWeight;
+}
+
+}
+return 0;
+}
+
+Weight::Weight() noexcept {
+bIsKnown = false;
+bHasMax = false;
+unitOfWeight = POUND;
+maxWeight = UNKNOWN_WEIGHT;
+}
+
+///Weight::Weight( const UnitOfWeight newUnitOfWeight, const float newMaxWeight ) : Weight( newUnitOfWeight ) {
+///  setMaxWeight( newMaxWeight );
+/// assert( validate() );
+///}
+
+Weight::Weight(float newWeight) {
+    bIsKnown = false;
+    bHasMax = false;
+    unitOfWeight = POUND;
+    weight = newWeight;
+    maxWeight = UNKNOWN_WEIGHT;
+}
+
+bool Weight::isWeightKnown() const noexcept {
+return bIsKnown;
+}
+
+bool Weight::validate() const noexcept {
+//float checkWeight;
+assert(validate());
+return true;
+}
+
+float Weight::getWeight(Weight::UnitOfWeight weightUnits) const noexcept{
+if(weight == 0){
+return UNKNOWN_WEIGHT;
+}
+assert(validate());
+float convertedWeight = convertWeight(weight, unitOfWeight, weightUnits);
+return convertedWeight;
+}
+
+float Weight::getMaxWeight() const noexcept {
+///if(!isWeightKnown())
+if(validate()) {
+return maxWeight;
+}else{
+return UNKNOWN_WEIGHT;
+}
+}
+
+void Weight::setMaxWeight(float newMaxWeight) {
+    maxWeight = newMaxWeight;
+}
+
+///bool Weight::isWeightValid(float checkWeight) const noexcept {
+/////  assert( (checkWeight > 0) || checkWeight == UNKNOWN_WEIGHT );
+///if( bHasMax ){
+////  assert( checkWeight <= maxWeight );
+///}
+///return true;
+///}
+
+
+Weight::Weight(float newWeight, float newMaxWeight) {
+    bIsKnown = false;
+    bHasMax = false;
+    unitOfWeight = POUND;
+    weight = newWeight;
+    maxWeight = UNKNOWN_WEIGHT;
+}
+
+Weight::Weight(float newWeight, UnitOfWeight newUnitOfWeight, float newMaxWeight) {
+    bIsKnown = true;
+    bHasMax = true;
+    unitOfWeight = newUnitOfWeight;
+    weight = newWeight;
+    maxWeight = newMaxWeight;
+}
+
+
+////getters
+float Weight::getWeight() const noexcept{
+assert(validate());
+return weight;
+}
+bool Weight::hasMaxWeight() const noexcept {
+
+return bHasMax;
+}
+///////////////////////////////////////////////////////
+
+std::ostream& operator<<( ostream& lhs_stream
+        ,const Weight::UnitOfWeight rhs_UnitOfWeight ) {
+    switch( rhs_UnitOfWeight ) {
+        case Weight::POUND: return lhs_stream << POUND_LABEL ;
+        case Weight::KILO:  return lhs_stream << KILO_LABEL ;
+        case Weight::SLUG:  return lhs_stream << SLUG_LABEL ;
+        default:
+            throw out_of_range( "The unit can’t be mapped to a string" );
+    }
+}
+
+bool Weight::operator==(const Weight &rhs_Weight) const {
+
+    float lhs_weight = bIsKnown ? getWeight(Weight::POUND) : 0;
+    float rhs_weight = (rhs_Weight.bIsKnown) ? rhs_Weight.getWeight(Weight::POUND) : 0;
+    return lhs_weight == rhs_weight;
+}
+
+Weight &Weight::operator+=(float rhs_addToWeight) {
+    weight += rhs_addToWeight;
+    return *this;
+}
+
+void Weight::dump() const noexcept {
+cout << "==============================================" << endl;
+cout << "bIsKnown: " << bIsKnown << endl;
+cout << "bHasMax: " << bHasMax << endl;
+cout << "Unit of Weight: " << unitOfWeight << endl;
+cout << "Weight: " << weight << endl;
+cout << "Max Weight: " << maxWeight << endl;
+}
---
+++ Weight.h
@@ -0,0 +1,93 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205-lab12a-fatCat - EE 205 - Spr 2022
+///
+/// @file Weight.h
+/// @version 1.0
+///
+/// @author Dane Sears <dsears@hawaii.edu>
+/// @date   16_APR_2022
+///////////////////////////////////////////////////////////////////////////////
+#include <string>
+#include <ostream>
+
+using namespace std;
+
+///float maxWeight;
+
+class Weight {
+public:  ////////////// Enumerations //////////////
+    /// A unit of measure for weight
+    // static const std::string POUND_LABEL = "Pound" ;
+//static const std::string KILO_LABEL = "Kilo";
+//static const std::string SLUG_LABEL = "Slug";
+
+    static float fromKilogramToPound(float kilogram) noexcept;
+    static float fromPoundToKilogram(float pound) noexcept;
+    static float fromSlugToPound(float slug) noexcept;
+    static float fromPoundToSlug(float pound) noexcept;
+
+    enum UnitOfWeight {
+        POUND, KILO, SLUG
+    };
+
+    enum UnitOfWeight unitOfWeight;
+
+//private:
+    //  bool bIsKnown;
+    //bool bHasMax;
+
+public:
+    float weightInPounds;
+    float weightInSlugs;
+    float weighInKilos;
+    bool bIsKnown;
+    bool bHasMax;
+    float maxWeight;
+    float weight;
+    float checkWeight;
+    float convertedWeight;
+
+public:
+    Weight() noexcept;
+    Weight(float newWeight) ;
+    Weight(UnitOfWeight newUnitOfMeasure) noexcept;
+    Weight(float newWeight, UnitOfWeight newUnitOfMeasure);
+    Weight(float newWeight, float newMaxWeight);
+    Weight(UnitOfWeight  newUnitOfMeasure, float newMaxWeight);
+    Weight(float newWeight, UnitOfWeight newUnitOfMeasure, float newMaxWeight);
+
+public:
+    static float convertWeight();
+    bool isWeightKnown() const noexcept;
+    bool hasMaxWeight() const noexcept;
+    bool validate () const noexcept;
+    bool isWeightValid(float checkWeight) noexcept;
+    float getWeight() const noexcept;
+    float getWeight(UnitOfWeight weightUnits) const noexcept;
+    float getMaxWeight() const noexcept;
+    bool operator==(const Weight &rhs_Weight) const;
+    Weight & operator+=(float rhs_addToWeight);
+
+    void setWeight(float newWeight);
+    void setWeight(float newWeight, UnitOfWeight weightUnits);
+    void setMaxWeight(float newMaxWeight);
+    void dump() const noexcept;
+
+    //bool isWeightValid(float checkWeight, float maxWeight);
+    bool isWeightValid(float checkWeight) const;
+
+public:
+    static float convertWeight( float fromWeight, Weight::UnitOfWeight fromUnit,Weight::UnitOfWeight toUnit ) noexcept;
+
+    ///float getWeight(UnitOfWeight weightUnits);
+};
+
+
+
+
+
+//float Weight::convertWeight(float fromWeight, Weight::UnitOfWeight fromUnit, Weight::UnitOfWeight toUnit) noexcept {
+
+//}
+
