d806036718a1fabf5191d9faf54c38ff96c0da4a
daneFram <dsears@hawaii.edu>
2022-04-24 13:14:25 -1000

    finished proto of node. got rid of node.cpp

--- Node.h
+++ Node.h
@@ -18,9 +18,22 @@ friend class List;
 friend class SinglyLinkedList;
 
 public:
-    virtual void dump() const;
-    virtual bool validate() const noexcept;
-    virtual bool operator>(const Node &rightSide);
+    virtual void dump() const{
+        FORMAT_LINE_FOR_DUMP( "Node", "this" ) << this << std::endl;
+        FORMAT_LINE_FOR_DUMP( "Node", "next" ) << next << std::endl;
+    }
+    ///checks the health of the node
+    virtual bool validate() const noexcept{
+        if(nullptr == next){
+return true;
+        }
+        return false;
+    }
+    virtual bool operator>(const Node &rightSide){
+if(leftSide> rightSide){
+return true;
+}
+    }
 
 protected:
     Node* next = nullptr;
